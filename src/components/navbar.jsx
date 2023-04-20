c85233b4c763422815d797299f11231b2eea9e5f
Antony Gakuru <antony123muriithi@gmail.com>
2023-04-20 17:24:56 +0300

    fix logo link

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,10 +6,10 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-md bg-body-tertiary mx-auto ">
             <div className="container">
-                <a className="navbar-brand d-flex flex-column justify-content-start align-items-center gap-0" href="/">
+                <Link to="/" className="navbar-brand d-flex flex-column justify-content-start align-items-center gap-0" href="/">
                     <img src={Logo} href="Logo" width="150" height="40" className='m-0'/>
                     <p className="h4" style={{color: "#A0A09F"}}>foundation</p>
-                </a>
+                </Link>
                 <button className="navbar-toggler collapsed d-flex d-md-none flex-column justify-content-around" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="toggler-icon top-bar"></span>
                     <span className="toggler-icon middle-bar"></span>
