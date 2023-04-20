e6035f720f135f9c29906493888faed090fe0b6f
Antony Gakuru <antony123muriithi@gmail.com>
2023-04-20 16:36:09 +0300

    about page boilerplate

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import * as React from "react"
+// import { Link } from "gatsby"
 
-function about() {
-  return (
-    <div>about</div>
-  )
-}
+import Layout from "../components/layout"
+import Seo from "../components/seo";
 
-export default about
+
+
+const AboutPage = () => (
+  <Layout>
+    <div>
+        About Page Contents Here
+    </div>
+  </Layout>
+)
+
+/**
+ * Head export to define metadata for the page
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
+ */
+export const Head = () => <Seo title="About" />
+
+export default AboutPage
