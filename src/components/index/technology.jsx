74db10b9fb6f223390175107784d4e3f8b245557
Antony Gakuru <antony123muriithi@gmail.com>
2023-04-21 19:36:44 +0300

    add technology

--- src/components/index/technology.jsx
+++ src/components/index/technology.jsx
@@ -7,6 +7,15 @@ function Technology() {
                 Technology
             </p>
             <h2 className="text-capitalize text-center fw-bold">The era of social networking</h2>
+            <div className="pictorama position-relative">
+                <div className="tech-image img1"></div>
+                <div className="tech-image img2"></div>
+                <div className="tech-image img3"></div>
+                <div className="tech-image img4"></div>
+                <div className="tech-image img5"></div>
+                <div className="tech-image img6"></div>
+
+            </div>
         </div>
     )
 }
