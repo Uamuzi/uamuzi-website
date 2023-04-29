219bc3f35b329188f09642749fad216cbb81d5d9
Antony Gakuru <antony123muriithi@gmail.com>
2023-04-29 13:13:21 +0300

    add dropdown

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,8 +11,8 @@ function Navbar() {
         <nav className="navbar navbar-expand-md bg-body-tertiary mx-auto ">
             <div className="container">
                 <Link to="/" className="navbar-brand d-flex flex-column justify-content-start align-items-center gap-0" href="/">
-                    <img src={Logo} href="Logo" width="150" height="40" className='m-0'/>
-                    <p className="h4" style={{color: "#A0A09F"}}>foundation</p>
+                    <img src={Logo} href="Logo" width="150" height="40" className='m-0' />
+                    <p className="h4" style={{ color: "#A0A09F" }}>foundation</p>
                 </Link>
                 <button className="navbar-toggler collapsed d-flex d-md-none flex-column justify-content-around" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="toggler-icon top-bar"></span>
@@ -25,20 +25,15 @@ function Navbar() {
                         <Link to="/about" className='nav-link'>About Us</Link>
                         <Link className="nav-link" to="/team">Our Team</Link>
                         <Link className="nav-link" to="/pillars">Pillars</Link>
-                        {/* <Link className="nav-link" to="/innovation">Innovation</Link> */}
-                        {/* <Link className="nav-link" to="/hub">Hub</Link> */}
-                        <div className='nav-link p-link' style={{position: 'relative'}} onClick={handleOpen}>Hub <box-icon type='solid' name='chevron-down'></box-icon></div>
-                        {open ? (
-                            <div style={{
-                                position: 'absolute',
-                                top: '45px',
-                                left: '60%'
-
-                            }}>
-                                <Link className='nav-link' to='/innovation'>Innovation</Link>
-                                <Link className='nav-link' to='/hub'>Academy</Link>
-                            </div>
-                        ) : null}
+                        <li class="nav-item dropdown">
+                            <Link class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Hub
+                            </Link>
+                            <ul class="dropdown-menu">
+                                <li className='drop-li'><Link class="dropdown-item drop-link" href="/innovation">Innovation</Link></li>
+                                <li className='drop-li'><Link class="dropdown-item drop-link" href="/hub">Academy</Link></li>
+                            </ul>
+                        </li>
                         <Link className="nav-link" to="/volunteers">Volunteers</Link>
                         <Link to="/contact" className="nav-link">Contact Us</Link>
                     </div>
