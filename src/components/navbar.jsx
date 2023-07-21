789cf0c20d3530b15429f4412d19d57521156866
Antony Muriithi Gakuru <76877744+morehwachege@users.noreply.github.com>
2023-07-21 16:31:05 +0300

    Revert "Navigation Toggle"

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react'
 import Logo from "../images/logo-uamuzi.png";
-import { Link } from "gatsby";
+import { Link } from "gatsby"
 
 function Navbar() {
-   const [isNavOpen, setIsNavOpen] = useState(false);
-
-   const handleNavToggle = () => {
-     setIsNavOpen(!isNavOpen);
-   };
     
     return (
         <nav className="navbar navbar-expand-md bg-body-tertiary mx-auto sticky-top bg-light">
@@ -16,18 +11,18 @@ function Navbar() {
                     <img src={Logo} href="Logo" width="150" height="40" className='m-0' alt='Uamuzi Logo' />
                     <p className="h4" style={{ color: "#A0A09F" }}>foundation</p>
                 </Link>
-                <button className="navbar-toggler collapsed d-flex d-md-none flex-column justify-content-around" onClick={handleNavToggle} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler collapsed d-flex d-md-none flex-column justify-content-around" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="toggler-icon top-bar"></span>
                     <span className="toggler-icon middle-bar"></span>
                     <span className="toggler-icon bottom-bar"></span>
                 </button>
-                <div className={`collapse navbar-collapse mx-auto ${isNavOpen ? 'show' : ''}`} id="navbarNavAltMarkup">
+                <div className="collapse navbar-collapse mx-auto" id="navbarNavAltMarkup">
                     <div className="navbar-nav mx-auto gap-3 ">
-                        <Link to="/" className='nav-link' activeClassName="active" aria-current="page" onClick={handleNavToggle}>Home</Link>
-                        <Link to="/about" className='nav-link' activeClassName="active" onClick={handleNavToggle}>About Us</Link>
-                        <Link className="nav-link" activeClassName="active" to="/team" onClick={handleNavToggle}>Our Team</Link>
-                        <Link className="nav-link" activeClassName="active" to="/pillars" onClick={handleNavToggle}>Pillars</Link>
-                        <li className="nav-item dropdown" activeClassName="active" onClick={handleNavToggle}>
+                        <Link to="/" className='nav-link active' aria-current="page">Home</Link>
+                        <Link to="/about" className='nav-link'>About Us</Link>
+                        <Link className="nav-link" to="/team">Our Team</Link>
+                        <Link className="nav-link" to="/pillars">Pillars</Link>
+                        <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Hub
                             </a>
@@ -36,8 +31,8 @@ function Navbar() {
                                 <li className='drop-li'><Link className="dropdown-item drop-link" to="/innovation">Innovation</Link></li>
                             </ul>
                         </li>
-                        <Link className="nav-link" activeClassName="active" to="/volunteers">Volunteers</Link>
-                        <Link to="/contact" activeClassName="active" className="nav-link">Contact Us</Link>
+                        <Link className="nav-link" to="/volunteers">Volunteers</Link>
+                        <Link to="/contact" className="nav-link">Contact Us</Link>
                     </div>
                 </div>
             </div>
