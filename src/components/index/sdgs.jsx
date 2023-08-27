93f10f51114e89e3607957b5db21abb3a3846d3e
Gtindi <tindigabriel@gmail.com>
2023-08-27 22:49:58 +0300

    Implement addition of popup screen within SDG's section

--- src/components/index/sdgs.jsx
+++ src/components/index/sdgs.jsx
@@ -4,21 +4,55 @@ import Layout from '../layout'
 function SDGs() {
     const [open, setOpen] = React.useState(false);
 
-    // if (open) {
-    //     return (
-    //             <div className="pop_up">
-    //                 <div className="icon">
-    //                     <box-icon style={{cursor: 'pointer'}} onClick={() => setOpen(false)} color='#8067AD' name='x'></box-icon>
-    //                 </div>
-    //                 <div className="line" />
-    //                 <h5 className="event text-center py-2">Civic Engagement</h5>
-                    
-    //                 <div className="close">
-    //                     <button onClick={() => setOpen(false)}>Close</button>
-    //                 </div>
-    //             </div>
-    //     )
-    // }
+    if (open) {
+        return (
+                <div className="pop_up">
+                    <div className="icon">
+                        <box-icon style={{cursor: 'pointer'}} onClick={() => setOpen(false)} color='#8067AD' name='x'></box-icon>
+                    </div>
+                    <div className="line" />
+                    <h5 className="event text-center py-2">Our SDG's</h5>
+                    <ul className="landing-goals m-0 p-0">
+                        <li className="goals d-flex gap-2">
+                            <span className='check-icon'>
+                                <box-icon name='check-circle' size="sm" color="#8067AD" ></box-icon>
+                            </span>
+                            <p><span className="primary-color" > Goal 5:</span> <b>Gender Equality</b> - We ensures women full and effective participation and equal opportunities for leadership at all levels of decision making in political, economic and public life.
+                            </p>
+                        </li>
+                        <li className="goals d-flex gap-2">
+                            <span className='check-icon'>
+                                <box-icon name='check-circle' size="sm" color="#8067AD" ></box-icon>
+                            </span>
+                            <p><span className="primary-color" > Goal 9:</span> <b>Industry, Innovation and Infrastructure</b> - We significantly increases access to information and communication and strive to provide affordable and equal access to technology for everyone.
+                            </p>
+                        </li><li className="goals d-flex gap-2">
+                            <span className='check-icon'>
+                                <box-icon name='check-circle' size="sm" color="#8067AD"></box-icon>
+                            </span>
+                            <p><span className="primary-color" > Goal 13:</span> <b> Climate Action</b> - We conduct civic education and raise awareness on climate change mitigation, adaptation, impact reduction and early warning.
+                            </p>
+                        </li>
+                        <li className="goals d-flex gap-2">
+                            <span className='check-icon'>
+                                <box-icon name='check-circle' size="sm" color="#8067AD" ></box-icon>
+                            </span>
+                            <p><span className="primary-color" > Goal 16:</span> <b>Peace, Justice and Strong institutions</b> - We conduct civic education and raise awareness on climate change mitigation, adaptation, impact reduction and early warning.
+                            </p>
+                        </li>
+                        <li className="goals d-flex gap-2">
+                            <span className='check-icon'>
+                                <box-icon name='check-circle' size="sm" color="#8067AD" ></box-icon>
+                            </span>
+                            <p><span className="primary-color" > Goal 17:</span> <b>Partnership for the goal</b> - We enhance support for implementing effective and targeted capacity-building to support local, national and international plans to implement the sustainable development goals.                            </p>
+                        </li>
+                    </ul>
+                    <div className="close">
+                        <button onClick={() => setOpen(false)}>Close</button>
+                    </div>
+                </div>
+        )
+    }
     return (
         <div className="container py-5">
             <div className="row px-3">
@@ -60,7 +94,7 @@ function SDGs() {
                             <p><span className="primary-color" > Goal 13:</span> <b> Climate Action</b> - We conduct civic education and raise awareness on climate change mitigation, adaptation, impact reduction and early warning.
                             </p>
                         </li>
-                        <button className="btn-primary">Show More
+                        <button className="btn-primary" onClick={() => setOpen(true)}>Show More
                             <box-icon className="hero-icon-btn" name='right-arrow-alt' size="sm" color="#8067AD"></box-icon>
                         </button>
                     </ul>
