ef25a0f691d99083365f6078758b619d061877b3
Antony Gakuru <antony123muriithi@gmail.com>
2023-12-03 03:09:42 +0300

    updatre uri

---
+++ public/component---src-pages-page-2-js-f8ccd7fb5c891e48f4ba.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkuamuzi=self.webpackChunkuamuzi||[]).push([[617],{4948:function(e,t,n){n.r(t),n.d(t,{Head:function(){return o}});var a=n(7294),l=n(1883),u=n(5701),c=n(9357);const o=()=>a.createElement(c.Z,{title:"Page two"});t.default=()=>a.createElement(u.Z,null,a.createElement("h1",null,"Hi from the second page"),a.createElement("p",null,"Welcome to page 2"),a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-f8ccd7fb5c891e48f4ba.js.map
