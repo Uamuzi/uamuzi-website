c1b7f4b6bc2de7a49e1a178f6744ddab2e95a02c
Monicah-Ngari <wakarimangari@gmail.com>
2024-08-16 13:51:09 +0300

    added new team members

--- src/components/index/landingTeam.jsx
+++ src/components/index/landingTeam.jsx
@@ -1,108 +1,199 @@
-import React from 'react'
+import React from "react"
 
 import { StaticImage } from "gatsby-plugin-image"
 
-import { Link } from 'gatsby';
-
+import { Link } from "gatsby"
 
 function LandingTeam() {
-    
-    return (
-        <div className='container py-5 px-2'>
-            <div className="row">
-                <div className="col-12 col-sm-12 col-md-6 col-lg-6 pt-3">
-                    <p className="p text-uppercase fs-6 py-2 sdg-top fw-bold">
-                        Our amazing team
-                    </p>
-                    <h4 className="h4 sdg-title fw-bold py-2 text-capitalize">
-                        Meet the team
-                    </h4>
-                    <p className="p sdg-paragraph fw-light py-2 pr-3">
-                        Our leadership team drives the strategy and direction of our work to deliver impact at scale for the most vulnerable.
-                    </p>
-                </div>
-                <div className="col-12 col-sm-12 col-md-6 col-lg-6 d-flex gap-5 justify-content-center align-items-center flex-wrap pt-3">
-                    <div className="team-inner m-0 p-0 position-relative">
-                        <StaticImage className='team-img' src="../../images/Elvis.jpg" placeholder="blurred" />
-                        <div className="long-purple-line position-absolute"></div>
-                            <div className="short-grey-line position-absolute"></div>
-                       
-                        <br />
-                        <div className="team-member-info text-center py-2">
-                            <p className="text-capitalize fw-bold text-red-500">Elvis Sitati</p>
-                            <p className="primary-color text-capitalize">Founder & Chief Executive Officer</p>
-                            <span className="primary-color d-flex gap-3 flex-nowrap justify-content-center align-items-center">
-                                <a href="https://www.linkedin.com/in/elvis-sitati-2a282b79" rel="noopener noreferrer" target='_blank'><box-icon name='linkedin-square' type='logo' color="#8067AD" ></box-icon></a>
-                                <a href="mailto:elvis.sitati@uamuzi.org"><box-icon name='envelope' type='solid' color="#8067AD" ></box-icon></a>
-                            </span>
-
-                        </div>
-                    </div>
-                    <div className="team-inner m-0 p-0 position-relative">
-                    <StaticImage className='team-img' src="../../images/Bisagaya.jpg" placeholder="blurred" />
-                            <div className="long-purple-line position-absolute"></div>
-                            <div className="short-grey-line position-absolute"></div>
-                       
-                        <div className="team-member-info text-center py-2">
-                            <p className="text-capitalize fw-bold">Andrew Bisagaya</p>
-                            <p className="primary-color text-capitalize">Legal</p>
-                            <span className="primary-color">
-                                <a href="https://www.linkedin.com/in/bisagaya-andrew-309b9497" rel="noopener noreferrer" target='_blank'><box-icon name='linkedin-square' type='logo' color="#8067AD" ></box-icon></a>
-                            </span>
-
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-12 col-sm-12 col-md-12 col-lg-12 d-flex gap-5 justify-content-center align-items-center flex-wrap pt-5 ">
-                    {/* <Link to="/about" className="see-all fw-bold text-capitalize d-flex justify-content-center align-items-center">See all members <span ><box-icon name='right-arrow-alt' color="#8067AD" className="rogue"></box-icon></span></Link> */}
-                    <div className="team-inner m-0 p-0 position-relative">
-                    <StaticImage className='team-img' src="../../images/MonicaKimani.jpg" placeholder="blurred" />
-                            <div className="long-purple-line position-absolute"></div>
-                            <div className="short-grey-line position-absolute"></div>
-                        
-    <div className="team-member-info text-center py-2">
-                            <p className="text-capitalize fw-bold">Monicah Kimani</p>
-                            <p className="primary-color text-capitalize">Marketing</p>
-                            <span className="primary-color">
-                                <a href="https://www.linkedin.com/in/monicah-kimani-561a1b69" rel="noreferrer noopener" target='_blank'><box-icon name='linkedin-square' type='logo' color="#8067AD" ></box-icon></a>
-                            </span>
-
-                        </div>
-                    </div><div className="team-inner m-0 p-0 position-relative">
-                    <StaticImage className='team-img' src="../../images/Novat.jpg" placeholder="blurred" />
-                            <div className="long-purple-line position-absolute"></div>
-                            <div className="short-grey-line position-absolute"></div>
-                        
-                        <div className="team-member-info text-center py-2">
-                            <p className="text-capitalize fw-bold">Novat Matoju</p>
-                            <p className="primary-color text-capitalize">Finance </p>
-                            <span className="primary-color">
-                                <a href="https://www.linkedin.com/in/novatmatoju" rel="noopener noreferrer" target='_blank'><box-icon name='linkedin-square' type='logo' color="#8067AD" ></box-icon></a>
-                            </span>
-
-                        </div>
-                    </div><div className="team-inner m-0 p-0 position-relative">
-                    <StaticImage className='team-img' src="../../images/SharonO.png" placeholder="blurred" />
-                            <div className="long-purple-line position-absolute"></div>
-                            <div className="short-grey-line position-absolute"></div>
-                        
-                        <div className="team-member-info text-center py-2">
-                            <p className="text-capitalize fw-bold">Sharon Ochieng</p>
-                            <p className="primary-color text-capitalize">Programs </p>
-                            <span className="primary-color">
-                                <a href="https://www.linkedin.com/in/sharon-ochieng-2167757a" rel="noopener noreferrer" target='_blank'><box-icon name='linkedin-square' type='logo' color="#8067AD" ></box-icon></a>
-                            </span>
-
-                        </div>
-                    </div>
-                    <Link to="/team" className="see-all members align-items-center fw-bold text-capitalize d-flex justify-content-center align-items-center"> See all members <div className='top-right'>
-                        <box-icon name='right-arrow-alt' color="#8067AD" className="rogue top-right"></box-icon>
-                    </div></Link>
-                </div>
-            </div>
+  return (
+    <div className="container py-5 px-2">
+      <div className="row">
+        <div className="col-12 col-sm-12 col-md-6 col-lg-6 pt-3">
+          <p className="p text-uppercase fs-6 py-2 sdg-top fw-bold">
+            Our amazing team
+          </p>
+          <h4 className="h4 sdg-title fw-bold py-2 text-capitalize">
+            Meet the team
+          </h4>
+          <p className="p sdg-paragraph fw-light py-2 pr-3">
+            Our leadership team drives the strategy and direction of our work to
+            deliver impact at scale for the most vulnerable.
+          </p>
         </div>
-    )
+        <div className="col-12 col-sm-12 col-md-6 col-lg-6 d-flex gap-5 justify-content-center align-items-center flex-wrap pt-3">
+          <div className="team-inner m-0 p-0 position-relative">
+            <StaticImage
+              className="team-img"
+              src="../../images/Elvis.jpg"
+              placeholder="blurred"
+            />
+            <div className="long-purple-line position-absolute"></div>
+            <div className="short-grey-line position-absolute"></div>
+
+            <br />
+            <div className="team-member-info text-center py-2">
+              <p className="text-capitalize fw-bold text-red-500">
+                Elvis Sitati
+              </p>
+              <p className="primary-color text-capitalize">
+                Founder & Chief Executive Officer
+              </p>
+              <span className="primary-color d-flex gap-3 flex-nowrap justify-content-center align-items-center">
+                <a
+                  href="https://www.linkedin.com/in/elvis-sitati-2a282b79"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <box-icon
+                    name="linkedin-square"
+                    type="logo"
+                    color="#8067AD"
+                  ></box-icon>
+                </a>
+                <a href="mailto:elvis.sitati@uamuzi.org">
+                  <box-icon
+                    name="envelope"
+                    type="solid"
+                    color="#8067AD"
+                  ></box-icon>
+                </a>
+              </span>
+            </div>
+          </div>
+          {/* <div className="team-inner m-0 p-0 position-relative">
+            <StaticImage
+              className="team-img"
+              src="../../images/SharonW.jpg"
+              placeholder="blurred"
+            />
+            <div className="long-purple-line position-absolute"></div>
+            <div className="short-grey-line position-absolute"></div>
+
+            <div className="team-member-info text-center py-2">
+              <p className="text-capitalize fw-bold">Sharon Wangui</p>
+              <p className="primary-color text-capitalize">
+                Administration and Programs
+              </p>
+              <span className="primary-color">
+                <a
+                  href="https://www.linkedin.com/in/sharon-wangui-799a67209/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <box-icon
+                    name="linkedin-square"
+                    type="logo"
+                    color="#8067AD"
+                  ></box-icon>
+                </a>
+              </span>
+            </div>
+          </div> */}
+        </div>
+
+        <div className="col-12 col-sm-12 col-md-12 col-lg-12 d-flex gap-5 justify-content-center align-items-center flex-wrap pt-5 ">
+          {/* <Link to="/about" className="see-all fw-bold text-capitalize d-flex justify-content-center align-items-center">See all members <span ><box-icon name='right-arrow-alt' color="#8067AD" className="rogue"></box-icon></span></Link> */}
+          <div className="team-inner m-0 p-0 position-relative">
+            <StaticImage
+              className="team-img"
+              src="../../images/EugeneRaburu.png"
+              placeholder="blurred"
+            />
+            <div className="long-purple-line position-absolute"></div>
+            <div className="short-grey-line position-absolute"></div>
+
+            <div className="team-member-info text-center py-2">
+              <p className="text-capitalize fw-bold">Eugene Raburu</p>
+              <p className="primary-color text-capitalize">Technology</p>
+              <span className="primary-color">
+                <a
+                  href="https://www.linkedin.com/in/eugene-raburu-kf/"
+                  rel="noreferrer noopener"
+                  target="_blank"
+                >
+                  <box-icon
+                    name="linkedin-square"
+                    type="logo"
+                    color="#8067AD"
+                  ></box-icon>
+                </a>
+              </span>
+            </div>
+          </div>
+          <div className="team-inner m-0 p-0 position-relative">
+            <StaticImage
+              className="team-img"
+              src="../../images/Monicah.jpg"
+              placeholder="blurred"
+            />
+            <div className="long-purple-line position-absolute"></div>
+            <div className="short-grey-line position-absolute"></div>
+
+            <div className="team-member-info text-center py-2">
+              <p className="text-capitalize fw-bold">Monicah Ngari</p>
+              <p className="primary-color text-capitalize">Frontend </p>
+              <span className="primary-color">
+                <a
+                  href="https://www.linkedin.com/in/monicah-ngari-96459321b/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <box-icon
+                    name="linkedin-square"
+                    type="logo"
+                    color="#8067AD"
+                  ></box-icon>
+                </a>
+              </span>
+            </div>
+          </div>
+          <div className="team-inner m-0 p-0 position-relative">
+            <StaticImage
+              className="team-img"
+              src="../../images/TimothyMabango.jpg"
+              placeholder="blurred"
+            />
+            <div className="long-purple-line position-absolute"></div>
+            <div className="short-grey-line position-absolute"></div>
+
+            <div className="team-member-info text-center py-2">
+              <p className="text-capitalize fw-bold">Timothy Mabango</p>
+              <p className="primary-color text-capitalize">
+                Partnership and Resource Mobilization{" "}
+              </p>
+              <span className="primary-color">
+                <a
+                  href="https://www.linkedin.com/in/mabango-timothy-capm/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <box-icon
+                    name="linkedin-square"
+                    type="logo"
+                    color="#8067AD"
+                  ></box-icon>
+                </a>
+              </span>
+            </div>
+          </div>
+          <Link
+            to="/team"
+            className="see-all members align-items-center fw-bold text-capitalize d-flex justify-content-center align-items-center"
+          >
+            {" "}
+            See all members{" "}
+            <div className="top-right">
+              <box-icon
+                name="right-arrow-alt"
+                color="#8067AD"
+                className="rogue top-right"
+              ></box-icon>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default LandingTeam;
+export default LandingTeam
--- src/pages/team.js
+++ src/pages/team.js
@@ -1,52 +1,147 @@
 import * as React from "react"
 
 import Layout from "../components/layout"
-import Seo from "../components/seo";
-import ImageDisplay from "../components/team/ImageDisplay";
+import Seo from "../components/seo"
+import ImageDisplay from "../components/team/ImageDisplay"
 
-const Elvis = 'Elvis.jpg'
-const CollinsOmondi = 'CollinsOmondi.jpg'
-const SamiWafula = 'SamiWafula.jpeg'
-const JeremyWachira = 'JeremyWachira.jpg'
-const TimothyMabango = 'TimothyMabango.jpg'
-const NjengaKarori = 'NjengaKarori.png'
-const JoanGichuhi = 'JoanGichuhi.jpg'
-const AntonyGakuru = 'AntonyGakuru.png'
-const MonicaKimani = 'MonicaKimani.jpg'
-const EugeneRaburu = 'EugeneRaburu.png'
-const Novat = 'Novat.jpg'
-const SharonO = 'SharonO.png'
-const Bisagaya = 'Bisagaya.jpg'
-
+const Elvis = "Elvis.jpg"
+const CollinsOmondi = "CollinsOmondi.jpg"
+const SamiWafula = "SamiWafula.jpeg"
+const JeremyWachira = "JeremyWachira.jpg"
+const TimothyMabango = "TimothyMabango.jpg"
+// const NjengaKarori = "NjengaKarori.png"
+// const JoanGichuhi = "JoanGichuhi.jpg"
+const AntonyGakuru = "AntonyGakuru.png"
+// const MonicaKimani = "MonicaKimani.jpg"
+const EugeneRaburu = "EugeneRaburu.png"
+// const Novat = "Novat.jpg"
+// const SharonO = "SharonO.png"
+// const Bisagaya = "Bisagaya.jpg"
+const Monicah = "Monicah.jpg"
+const Muliro = "Muliro.jpg"
+const Kibet = "Kibet.jpg"
+const Immanuel = "Immanuel.jpg"
 
 const TeamPage = () => (
-    <Layout>
-        <div className="container">
-            <p className="text-center event">OUR AMAZING TEAM</p>
-            <p className="text-center h3 event-title">Meet The Team</p>
-            <p className="text-center p">Our leadership team drives the strategy and direction of our work to deliver impact at scale for the most vulnerable.</p>
+  <Layout>
+    <div className="container">
+      <p className="text-center event">OUR AMAZING TEAM</p>
+      <p className="text-center h3 event-title">Meet The Team</p>
+      <p className="text-center p">
+        Our leadership team drives the strategy and direction of our work to
+        deliver impact at scale for the most vulnerable.
+      </p>
 
-            <div className="team py-4">
-                <ImageDisplay imageName={Elvis} fullName={"Elvis Sitati"} position={"Founder & Chief Executive Officer"} linkedinUsername={"elvis-sitati-2a282b79"} email={"elvis.sitati@uamuzi.org"} />
-                <ImageDisplay imageName={Bisagaya} fullName={"Andrew Bisagaya"} position={"Leagal"} linkedinUsername={"bisagaya-andrew-309b9497"} />
-                <ImageDisplay imageName={Novat} fullName={"Novat Matoju"} position={"Finance"} linkedinUsername={"novatmatoju"} />
-                <ImageDisplay imageName={SharonO} fullName={"Sharon Ochieng"} position={"Programs"} linkedinUsername={"sharon-ochieng-2167757a"} />
-                <ImageDisplay imageName={MonicaKimani} fullName={"Monica Kimani"} position={"Marketing"} linkedinUsername={"monicah-kimani-561a1b69"} />
-                <ImageDisplay imageName={JeremyWachira} fullName={"Jeremy Wachira"} position={"Volunteer Coordinator"} linkedinUsername={"jeremy-wachira-610a221b1"} />
+      <div className="team py-4">
+        <ImageDisplay
+          imageName={Elvis}
+          fullName={"Elvis Sitati"}
+          position={"Founder & Chief Executive Officer"}
+          linkedinUsername={"elvis-sitati-2a282b79"}
+          email={"elvis.sitati@uamuzi.org"}
+        />
+        {/* <ImageDisplay
+          imageName={KarenKurgat}
+          fullName={"Karen Kurgat"}
+          position={"Leagal"}
+          linkedinUsername={"karen-kurgat-a4b9511b9"}
+        /> */}
+        <ImageDisplay
+          imageName={Immanuel}
+          fullName={"Immanuel Nduthu"}
+          position={"Finance"}
+          linkedinUsername={"immanuel-nduthu-792535307"}
+        />
+        {/* <ImageDisplay
+          imageName={SharonW}
+          fullName={"Sharon Wangui"}
+          position={"Administration and Programs"}
+          linkedinUsername={"sharon-wangui-799a67209"}
+        /> */}
+        {/* <ImageDisplay
+          imageName={JoeSila}
+          fullName={"Joe Sila"}
+          position={"Marketing and Communication"}
+          linkedinUsername={"joe-sila-34823a267"}
+        /> */}
+        <ImageDisplay
+          imageName={JeremyWachira}
+          fullName={"Jeremy Wachira"}
+          position={"Volunteer Coordinator"}
+          linkedinUsername={"jeremy-wachira-610a221b1"}
+        />
 
-                <ImageDisplay imageName={EugeneRaburu} fullName={"Eugene Raburu"} position={"Technology"} linkedinUsername={"eugene-raburu-kf"} />
-                <ImageDisplay imageName={AntonyGakuru} fullName={"Antony Gakuru"} position={"Lead Engeneer"} linkedinUsername={"muriithigakuru"} />
+        <ImageDisplay
+          imageName={EugeneRaburu}
+          fullName={"Eugene Raburu"}
+          position={"Technology"}
+          linkedinUsername={"eugene-raburu-kf"}
+        />
+        <ImageDisplay
+          imageName={AntonyGakuru}
+          fullName={"Antony Gakuru"}
+          position={"Lead Engeneer"}
+          linkedinUsername={"muriithigakuru"}
+        />
 
-                <ImageDisplay imageName={NjengaKarori} fullName={"Njenga Karori"} position={"UI/UX Designer"} linkedinUsername={"njenga-karori-2b9bb6223"} />
-                <ImageDisplay imageName={JoanGichuhi} fullName={"Joan Gichuhi"} position={"Lead Designer"} linkedinUsername={"joan-gichuhi"} />
+        {/* <ImageDisplay
+          imageName={DennisWachira}
+          fullName={"Dennis Wachira"}
+          position={"Engineering Academy"}
+          linkedinUsername={"dennis-wachira-451786258"}
+        /> */}
 
-                <ImageDisplay imageName={CollinsOmondi} fullName={"Collins Omondi"} position={"Frontend"} linkedinUsername={"c-o-m-o-n"} />
-                <ImageDisplay imageName={SamiWafula} fullName={"Sami Wafula"} position={"Frontend"} linkedinUsername={"sami-wafula"} />
-                <ImageDisplay imageName={TimothyMabango} fullName={"Timothy Mabango"} position={"Resource Mobilization"} linkedinUsername={"mabango-timothy-capm-972343186/"} />
-
-            </div>
-        </div>
-    </Layout>
+        <ImageDisplay
+          imageName={CollinsOmondi}
+          fullName={"Collins Omondi"}
+          position={"Lead Frontend"}
+          linkedinUsername={"c-o-m-o-n"}
+        />
+        <ImageDisplay
+          imageName={SamiWafula}
+          fullName={"Sami Wafula"}
+          position={"Frontend"}
+          linkedinUsername={"sami-wafula"}
+        />
+        <ImageDisplay
+          imageName={TimothyMabango}
+          fullName={"Timothy Mabango"}
+          position={"Resource Mobilization"}
+          linkedinUsername={"mabango-timothy-capm-972343186"}
+        />
+        {/* <ImageDisplay
+          imageName={RodgersBiwott}
+          fullName={"Rodgers Biwott"}
+          position={"Lead Cloud and DevOps"}
+          linkedinUsername={"rodgersbiwott"}
+        /> */}
+        <ImageDisplay
+          imageName={Monicah}
+          fullName={"Monicah Ngari"}
+          position={"Frontend"}
+          linkedinUsername={"monicah-ngari-96459321b"}
+        />
+        <ImageDisplay
+          imageName={Muliro}
+          fullName={"Michael Muliro"}
+          position={"Backend"}
+          linkedinUsername={"michael-muliro-064365124"}
+        />
+        {/* <ImageDisplay
+          imageName={JanJuma}
+          fullName={"Jan Juma"}
+          position={"Cloud and DevOps"}
+          linkedinUsername={"jan-juma-4a1595229"}
+        /> */}
+        <ImageDisplay
+          imageName={Kibet}
+          fullName={"Ian Kibet,"}
+          position={"Artificial Intelligence and Machine Learning"}
+          linkedinUsername={"ian-kibet-"}
+        />
+      </div>
+    </div>
+  </Layout>
 )
 
 export const Head = () => <Seo title="Team" />
