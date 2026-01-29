49faa1d0150c5c6b16c651d3ca78d5431b19b5b3
Antony Gakuru <antony123muriithi@gmail.com>
2026-01-29 16:46:39 +0300

    add article title

--- src/pages/papersEdit.js
+++ src/pages/papersEdit.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import { ToastContainer, toast } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
@@ -7,79 +7,85 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import PapersEditRichText from "../components/PapersEditRichText"
 
-function PapersEdit() { 
-    const [title, setTitle] = useState("");
-    const [author, setAuthor] = useState("");
-    const [body, setBody] = useState("")
-    const [loading, setLoading] = useState(false)
+function PapersEdit() {
+  const [title, setTitle] = useState("")
+  const [author, setAuthor] = useState("")
+  const [body, setBody] = useState("")
+  const [loading, setLoading] = useState(false)
 
-    const handleSubmit = async e => {
-      e.preventDefault()
-      setLoading(true)
+  const handleSubmit = async e => {
+    e.preventDefault()
+    setLoading(true)
 
-      try {
-        const res = await fetch("http://localhost:3000/api/v1/blogs", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            title,
-            body, // HTML string
-            author,
-            status: 1
-          }),
+    try {
+      const res = await fetch("http://localhost:3000/api/v1/blogs", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          body, // HTML string
+          author,
+          status: 1,
+        }),
+      })
+
+      if (!res.ok) {
+        const error = await res.json()
+        toast.error(`An unexpected error occured. Please try again later.`, {
+          position: toast.POSITION.TOP_RIGHT,
         })
-
-        if (!res.ok) {
-          const error = await res.json();
-          toast.error(`An unexpected error occured. Please try again later.`, {
-              position: toast.POSITION.TOP_RIGHT
-          });
-          throw error;
-        }else
-        {
-          toast.success(`Article has been created successfully`, {
-            position: toast.POSITION.TOP_RIGHT
-        });
+        throw error
+      } else {
+        toast.success(`Article has been created successfully`, {
+          position: toast.POSITION.TOP_RIGHT,
+        })
         setTitle("")
         setBody("")
         setAuthor("")
-        }
-      } catch (err) {
-        console.error(err)
-        alert("Failed to create blog")
-      } finally {
-        setLoading(false)
       }
+    } catch (err) {
+      console.error(err)
+      alert("Failed to create blog")
+    } finally {
+      setLoading(false)
     }
+  }
   return (
-  <Layout>
-    <ToastContainer />
-    <form onSubmit={handleSubmit} className="contact_form">
-      <input
-        type="text"
-        placeholder="Article title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        required
-      />
-      <input
-        type="text"
-        placeholder="Article author"
-        value={author}
-        onChange={(e) => setAuthor(e.target.value)}
-        required
-      />
+    <Layout>
+      <ToastContainer />
+      <div className="pt-2"></div>
+      <h3 className="text-center fw-bold">Write your articles</h3>
+      <div className="w-full d-flex justify-center items-center">
+        <form onSubmit={handleSubmit} className="contact_form">
+          <input
+            type="text"
+            placeholder="Article title"
+            className="mt-2"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            required
+          />
+          <input
+            type="text"
+            placeholder="Article author"
+            className="my-2 w-[50%]"
+            value={author}
+            onChange={e => setAuthor(e.target.value)}
+            required
+          />
 
-      <PapersEditRichText value={body} onChange={setBody} />
+          <PapersEditRichText value={body} onChange={setBody} />
 
-      <button className="join" type="submit" disabled={loading}>
-        {loading ? "Saving..." : "Create Article"}
-      </button>
-    </form>
-  </Layout>
-)}
+          <button className="join mt-2" type="submit" disabled={loading}>
+            {loading ? "Saving..." : "Create Article"}
+          </button>
+        </form>
+      </div>
+    </Layout>
+  )
+}
 
 export const Head = () => <Seo title="Edit Papers" />
 
