c956a6b2a84bf380c48e549f6c86c7c94191f01a
Antony Gakuru <antony123muriithi@gmail.com>
2026-02-05 19:37:24 +0300

    add blogs node

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,16 +3,43 @@
  *
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
  */
-
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
-exports.createPages = async ({ actions }) => {
+exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
+
+  // 1️⃣ DSG page
   createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
     defer: true,
+  })
+
+  // 2️⃣ Blog pages from slugs
+  const result = await graphql(`
+    {
+      allBlogs {
+        nodes {
+          id
+          slug
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    throw result.errors
+  }
+
+  result.data.allBlogs.nodes.forEach(blog => {
+    createPage({
+      path: `/blogs/${blog.slug}`,
+      component: require.resolve("./src/templates/blog.js"),
+      context: {
+        id: blog.id,
+        slug: blog.slug,
+      },
+    })
   })
 }
