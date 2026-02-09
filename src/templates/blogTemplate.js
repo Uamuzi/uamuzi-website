a776ff1266e029df72729db33beef1ee97bdca96
Antony Gakuru <antony123muriithi@gmail.com>
2026-02-09 13:02:49 +0300

    center blog div

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,18 +1,20 @@
-import * as React from "react"
-import { Link, graphql } from "gatsby"
+import React, {useEffect} from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const BlogTemplate = ({ pageContext }) => {
-  const {blog} = pageContext
+  const { blog } = pageContext
+
+  console.log(blog, 'this is from the blog template')
   return (
     <Layout>
-      <h1>
-        {blog.title}
-      </h1>
-      <p>{blog.body}</p>
-      <Link to="/">Go back to the homepage</Link>
+      <div className="w-100 d-flex justify-content-center align-items-center">
+        <div className="bg-danger px-8 w-75 align-self-center ">
+          <h1>{blog.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: blog.body }} />
+        </div>
+      </div>
     </Layout>
   )
 }
