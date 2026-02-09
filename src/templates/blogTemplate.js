f09c4ec40a6df00d93adffeaff4226aa0bb5fbe5
Antony Gakuru <antony123muriithi@gmail.com>
2026-02-09 13:07:35 +0300

    add author

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -9,9 +9,12 @@ const BlogTemplate = ({ pageContext }) => {
   console.log(blog, 'this is from the blog template')
   return (
     <Layout>
-      <div className="w-100 d-flex justify-content-center align-items-center">
-        <div className="bg-danger px-8 w-75 align-self-center ">
-          <h1>{blog.title}</h1>
+      <div className="w-100 mt-4 d-flex justify-content-center align-items-center">
+        <div className="px-8 w-75 align-self-center ">
+          <h1 className="fw-bold">{blog.title}</h1>
+          <h6 className="fw-bold my-4">
+            by <i>{blog.author}</i>
+          </h6>
           <div dangerouslySetInnerHTML={{ __html: blog.body }} />
         </div>
       </div>
